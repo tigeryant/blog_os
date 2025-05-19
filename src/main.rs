5fbab7f600a1b1ad22d84bfb67030db41c0ac89f
John Davies <jd.other@hotmail.com>
2025-05-19 19:25:16 +0700

    Implemented new page mapper and frame allocator

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,17 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use blog_os::memory;
     use x86_64::{structures::paging::{Translate, Page}, VirtAddr};
+    use blog_os::memory::BootInfoFrameAllocator;
 
     println!("Hello World{}", "!");
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = memory::EmptyFrameAllocator;
+    // let mut frame_allocator = memory::EmptyFrameAllocator;
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
     // map an unused page
     let page = Page::containing_address(VirtAddr::new(0));
